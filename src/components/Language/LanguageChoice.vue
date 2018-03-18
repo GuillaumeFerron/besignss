afcefe2723a1a0105fb9c6b0038028cd016c63a8
GuillaumeFerron <guillaume.ferron2@gmail.com>
2018-03-18 16:56:13 -0400

    Creation of Language choice component and use it in Language page

---
+++ src/components/Language/LanguageChoice.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="language-choice">
+    <router-link :to="language | filteredUrl">{{language}}</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'language-choice',
+    props: [
+      'language'
+    ],
+    filters: {
+      filteredUrl: (string) => {
+        // Return only the two first letters of the language, lower cased
+        return string.toLowerCase().substr(0, 2)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- src/components/pages/Language.vue
+++ src/components/pages/Language.vue
@@ -1,11 +1,26 @@
 <template>
-  
+  <div class="language-container">
+    <language-choice v-for="item in availLanguages" :key="item" :language="item"/>
+  </div>
 </template>
 
 <script>
-export default {
-    name: "Language"
-}
+  import LanguageChoice from '@/components/Language/LanguageChoice'
+
+  export default {
+    name: 'Language',
+    data  () {
+      return {
+        availLanguages: [
+          'English',
+          'Français'
+        ]
+      }
+    },
+    components: {
+      LanguageChoice
+    }
+  }
 </script>
 
 <style>
