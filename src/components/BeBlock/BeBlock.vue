ac176996d8cc7e852541375b0f09a155566f108c
GuillaumeFerron <guillaume.ferron2@gmail.com>
2018-03-19 13:05:06 -0400

    Creation of the EMILIEN BOUEXEL block

---
+++ src/components/BeBlock/BeBlock.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="be-container">
+    <div class="be-name">EMILIEN BOÜEXEL</div>
+    <div class="be-logo">
+      <img src="" alt="Besignss' Logo"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'be-block'
+  }
+</script>
+
+<style lang="scss" scoped>
+  .be-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    height: 250px;
+    width: 100%;
+    background-color: #B3B3B3;
+    .be-name {
+      height: 50%;
+      width: 70%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: #001023;
+      font-family: Bebas Neue, sans-serif;
+      font-size: 50px;
+    }
+    .be-logo {
+      height: 50%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+</style>
--- src/components/pages/Landing.vue
+++ src/components/pages/Landing.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="landing-container">
+    <be-block/>
     <language/>
   </div>
 </template>
 
 <script>
   import Language from '@/components/Language/Language.vue'
+  import BeBlock from '@/components/BeBlock/BeBlock'
 
   export default {
     name: 'landing',
     components: {
+      BeBlock,
       Language
     }
   }
